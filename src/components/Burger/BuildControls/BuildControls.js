89d75a70626a1652ffbaa22a07efceea62fc54a3
mrci18 <cibo@matson.com>
2020-05-01 01:35:27 -0700

    Order summary and modal

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,6 +11,7 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
+        <p>Current Price: <strong>{props.price}</strong></p>
         {controls.map(ctrl => (
             <BuildControl 
                 key={ctrl.label} 
@@ -19,6 +20,11 @@ const buildControls = (props) => (
                 removed={() => props.ingredientRemoved(ctrl.type)}
                 disabled={props.disabled[ctrl.type]} />
         ))}
+        <button 
+            className={classes.OrderButton}
+            disabled={!props.purchasable}
+            onClick={props.ordered}>ORDER NOW
+        </button>
     </div>
 );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
-    salad: 0.5,
-    cheese: 0.4,
-    meat: 1.3,
-    bacon: 0.7
+    salad: 0.50,
+    cheese: 0.40,
+    meat: 1.30,
+    bacon: 0.70
 };
 
 class BurgerBuilder extends Component {
@@ -19,7 +21,18 @@ class BurgerBuilder extends Component {
             meat: 0
 
         },
-        totalPrice: 0.00
+        totalPrice: 4,
+        purchasable: false,
+        purchasing: false
+    }
+
+    updatePurchasedState (ingredients) {
+        const sum = Object.keys(ingredients).map(igKey => {
+            return ingredients[igKey];
+        }).reduce((curSum, curEle) => {
+            return curSum + curEle
+        },0);
+        this.setState({purchasable: sum > 0})
     }
     
     addIngredientHandler = (type) => {
@@ -29,10 +42,10 @@ class BurgerBuilder extends Component {
             ...this.state.ingredients
         }
         updatedIngredients[type] = updatedCount;
-        const priceAddition = INGREDIENT_PRICES[type]
-        const newPrice = this.state.price + priceAddition
-
-        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+        const priceAddition = INGREDIENT_PRICES[type];
+        const newPrice = this.state.totalPrice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchasedState(updatedIngredients);
     }
 
     removeIngredientHandler = (type) => {
@@ -49,8 +62,13 @@ class BurgerBuilder extends Component {
         const priceReduction = INGREDIENT_PRICES[type];
         const newPrice = this.state.totalPrice - priceReduction;
         
-        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchasedState(updatedIngredients);
 
+    }
+
+    purchaseHandler = () => {
+        this.setState({purchasing: true});
     }
     render() {
         const disabledInfo = {
@@ -62,11 +80,17 @@ class BurgerBuilder extends Component {
         }
         return (
             <Aux>
+                <Modal show={this.state.purchasing}>
+                    <OrderSummary ingredients={this.state.ingredients}/>
+                </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
-                    disabled={disabledInfo}/>
+                    disabled={disabledInfo}
+                    purchasable={this.state.purchasable}
+                    ordered={this.purchaseHandler}
+                    price={this.state.totalPrice}/>
             </Aux>
         );
     }
