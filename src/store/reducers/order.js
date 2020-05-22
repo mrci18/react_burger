9bed7186f9f16a39d18d6bad1d2bd524fd4bab3f
mrci18 <cibo@matson.com>
2020-05-21 21:15:08 -0700

    detached functions in switch statement

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import {updateObject} from '../utility';
 
 const initialState = {
     orders: [],
@@ -6,50 +7,58 @@ const initialState = {
     purchased: false
 };
 
+const purchaseInit= (state, action) => {
+    return updateObject(state, {purchased: false})
+    
+};
+
+const purchaseBurgerStart = (state, action) => {
+    return updateObject(state, {loading: true})
+
+};
+
+const purchaseBurgerSuccess = (state,action) => {
+    const newOrder = {
+        ...action.orderData,
+        id: action.orderId
+    }
+    return updateObject(state,{
+        loading: false,
+        purchased: true,
+        orders: state.orders.concat(newOrder)
+    });
+};
+
+const purchaseBurgerFail = (state) => {
+    return updateObject(state, {loading: false});
+}
+
+const fetchOrderStart= (state) => {
+    return updateObject(state, {loading: true});
+
+};
+
+const fetchOrderSucces = (state, action) => {
+    return updateObject(state, {orders: action.orders,loading: false});
+};
+
+const fetchOrderFail = (state) => {
+    return updateObject(state,{loading: false});
+};
+
 const reducer = (state =initialState, action) => {
     switch (action.type) {
-        case actionTypes.PURCHASE_INIT:
-            return {
-                ...state,
-                purchased: false
-            };
-        case actionTypes.PURCHASE_BURGER_START:
-            return {
-                ...state,
-                loading: true
-            }
-        case actionTypes.PURCHASE_BURGER_SUCCESS:
-            const newOrder = {
-                ...action.orderData,
-                id: action.orderId
-            }
-                return {
-                    ...state,
-                    loading: false,
-                    purchased: true,
-                    orders: state.orders.concat(newOrder)
-                };
-        case actionTypes.PURCHASE_BURGER_FAIL:
-            return {
-                ...state,
-                loading: false
-            };
-        case actionTypes.FETCH_ORDERS_START:
-            return {
-                ...state,
-                loading: true
-            }
-        case actionTypes.FETCH_ORDERS_SUCCESS:
-            return {
-                ...state,
-                orders: action.orders,
-                loading: false
-            }
-        case actionTypes.FETCH_ORDERS_FAIL:
-            return {
-                ...state,
-                loading: false
-            }
+        case actionTypes.PURCHASE_INIT: return purchaseInit(state, action);
+
+        case actionTypes.PURCHASE_BURGER_START: return purchaseBurgerStart(state,action);
+
+        case actionTypes.PURCHASE_BURGER_SUCCESS: return purchaseBurgerSuccess(state, action)
+
+
+        case actionTypes.PURCHASE_BURGER_FAIL: return purchaseBurgerFail(state);
+        case actionTypes.FETCH_ORDERS_START: return fetchOrderStart(state)
+        case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrderSucces(state, action)
+        case actionTypes.FETCH_ORDERS_FAIL: return fetchOrderFail(state)
         default:
             return state;
     }
